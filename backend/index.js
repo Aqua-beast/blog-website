a110c2f6a16173e63847c783733e8ef39642eb56
= <=>
2024-07-02 18:38:08 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 
 const corsOption = {
-    origin: 'http://localhost:3000',
+    origin: 'https://blog-website-ochre-six.vercel.app',
     credentials: true,
 }
 
